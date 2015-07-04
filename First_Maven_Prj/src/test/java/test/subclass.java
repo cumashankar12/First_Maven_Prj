3584f360bf4d69658155e5cf1a8447f22424f6ad
cumashankar12 <c.umashankar@gmail.com>
2015-07-04 12:50:14 -0500

    Change-Id: Ie1d8ebeed74b276b9a479902eafcca85a5bc951f

--- First_Maven_Prj/src/test/java/test/subclass.java
+++ First_Maven_Prj/src/test/java/test/subclass.java
@@ -1,13 +1,14 @@
 package test;
 
+import org.testng.annotations.Test;
+
 public class subclass extends mainprg {
 	public String var1 = "sub";
+	@Test()
+	
 public void subclas (){
 	System.out.println(this.var1);
 	System.out.println(super.var1);
 }
-public static void main (String args[]){
-	subclass sub1 = new subclass();
-	sub1.subclas();
-}
+
 }
---
+++ Initialize/src/main/java/Readpom/Readparam.java
@@ -0,0 +1,11 @@
+package Readpom;
+
+
+public class Readparam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
