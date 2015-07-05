5751c6606635ed39859e45cd41f3c3df21c55daa
cumashankar12 <c.umashankar@gmail.com>
2015-07-05 11:10:24 -0500

    Change-Id: I8cc0b9f5d8c0a8e85681f7091f4a8c25727ff5f0

---
+++ First_Maven_Prj/src/main/java/Readpom/Readparam.java
@@ -0,0 +1,85 @@
+package Readpom;
+import java.util.Map;
+import org.testng.TestNG;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Readparam {
+	
+	/*public Readparam(final String... args){
+		for(String arg : args) {
+			final String[] split = arg.split("=", 2);
+			if(split.length == 2 && null != split[0].trim()) { // configuration
+				data.put(split[0].trim(), split[1].trim());
+				if(split[0].trim().equalsIgnoreCase("suite-template")){
+					prjpath = split[0].trim();
+					System.out.println("Project path is - " + prjpath);
+				}
+				System.out.println("arguments - "+ split[0].trim() + "--" + split[1].trim());
+				
+			}
+		}
+		
+	}*/
+	public void xmlDocumentCreation(){
+		try{
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = docFactory.newDocumentBuilder();
+		Document document = builder.newDocument();
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+                                                System.in));
+                System.out.println("Enter the number of Element for XML files: - \n");
+                int num = Integer.parseInt(br.readLine());
+                System.out.println("Enter the root Element: \n");
+                String rootElement = br.readLine();
+                Element element = document.createElement(rootElement);
+                document.appendChild(element);
+                for (int i = 1; i <= num; i++) {
+                                System.out.println("Enter the Element:- \n");
+                                String strElement = br.readLine();
+                                System.out.println("Enter the Data: - \n");
+                                String strData = br.readLine();
+                                Element childElement = document.createElement(strElement);
+                                childElement.appendChild(document.createTextNode(strData));
+                                element.appendChild(childElement);
+                }
+                TransformerFactory transFactory = TransformerFactory.newInstance();
+                Transformer transformer = transFactory.newTransformer();
+                DOMSource domSource = new DOMSource(document);
+                StreamResult streamResult = new StreamResult(System.out);
+                transformer.transform(domSource, streamResult);
+				} catch (Exception e) {
+                System.out.println(e.getMessage());
+				}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//final Readparam readpar = new Readparam(args);
+		//TestNG testNG = new TestNG();
+		Map<String, String> data = new java.util.HashMap<String, String>();
+		String prjpath;
+		for(String arg : args) {
+			final String[] split = arg.split("=", 2);
+			if(split.length == 2 && null != split[0].trim()) { // configuration
+				data.put(split[0].trim(), split[1].trim());
+				if(split[0].trim().equalsIgnoreCase("suite-template")){
+					prjpath = split[0].trim();
+					System.out.println("Project path is - " + prjpath);
+				}
+				System.out.println("arguments - "+ split[0].trim() + "--" + split[1].trim());
+				
+			}
+		}
+	}
+
+}
---
+++ First_Maven_Prj/src/main/java/Readpom/storeargs.java
@@ -0,0 +1,5 @@
+package Readpom;
+
+public class storeargs {
+
+}
